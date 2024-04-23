6b4d43b739e20b64725dc5deefb6a50babb75495
yulia <yw6908@nyu.edu>
2024-04-23 11:49:53 +0800

    project-b-update-draft

--- Project-b-draft/js/script2.js
+++ Project-b-draft/js/script2.js
@@ -22,23 +22,6 @@ function drawText(textStr, centerX, y) {
   text(textStr, centerX, y); 
 }
 
-function drawHorrorText() {
-  fill(255, 0, 0);  
-  textSize(32);  
-  textStyle(BOLD);  
-  textAlign(CENTER, CENTER);  
-  textFont('Creepster'); 
-
-  text("Which course should you take right now?", width / 2, height - 85);
-}
-
-
-function pickRandomDay() {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  const index = floor(random(days.length));  
-  day = days[index];  
-}
-
 function drawCalendar() {
   const rectangleWidth = 100; 
   const rectangleHeight = 50; 
@@ -51,31 +34,5 @@ function drawCalendar() {
   fill(0);  
   textSize(14);
   textAlign(CENTER, CENTER);
-  text(day + "\n7AM", x + rectangleWidth / 2, y + rectangleHeight / 2);  
-}
-
-function drawSchedule() {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  const hours = ["7AM", "11AM", "1PM", "5PM"];
-  const scheduleWidth = 400; 
-  const scheduleHeight = 200; 
-  const startX = (width - scheduleWidth) / 2; 
-  const startY = (height - scheduleHeight) / 2; 
-  const cellWidth = scheduleWidth / days.length;
-  const cellHeight = scheduleHeight / hours.length;
-
-  textSize(12);
-  textAlign(CENTER, CENTER);
-  
-  for (let i = 0; i < days.length; i++) {
-    for (let j = 0; j < hours.length; j++) {
-      const x = startX + i * cellWidth;
-      const y = startY + j * cellHeight;
-      fill(255);  
-      stroke(0);  
-      rect(x, y, cellWidth, cellHeight);
-      fill(0);  
-      text(days[i] + "\n" + hours[j], x + cellWidth / 2, y + cellHeight / 2);
-    }
-  }
+  text(day + "\n9AM", x + rectangleWidth / 2, y + rectangleHeight / 2);  
 }
